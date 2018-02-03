74ac521c1af1bf17f0e8c9dbf52be66d075191ae
emolytpic <emolyptic@gmail.com>
2018-02-03 08:57:49 -0700

    Fixed up the end of the Algorithm. Changed a foreach loop to a for loop because of lack of IEnumerable in my custom class. Moving onto Testing

--- PluralsightTest/PluralsightTest/Program.cs
+++ PluralsightTest/PluralsightTest/Program.cs
@@ -1,26 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PluralsightTest
 {
-    public class Node
+    public class node 
     {
         public string program;
         public string dependency;
-        public Node(string pro, string dep)
+        public node(string pro, string dep)
         {
             program = pro;
             dependency = dep;
         }
+
     }
     class Program
     {
         static void Main(string[] args)
         {
-            string[] testArray = new string[]
+            string[] validOne = new string[]
                 {
                 "KittenService: ",
                 "Leetmeme: Cyberportal",
@@ -30,6 +32,16 @@ namespace PluralsightTest
                 "Ice: "
                 };
 
+            string[] invalidOne = new string[]
+            {
+            "KittenService: ",
+            "Leetmeme: Cyberportal",
+            "Cyberportal: Ice",
+            "CamelCaser: KittenService",
+            "Fraudstream: ",
+            "Ice: Leetmeme"
+            };
+            
             //Test
             // Cycle;
             // Large Cycle;
@@ -37,8 +49,9 @@ namespace PluralsightTest
             // One element
             // 
 
-            string output = createDependencyList(testArray);
+            string output = createDependencyList(validOne);
 
+            Console.Write(output);
             Console.ReadLine();
         }
         public static string createDependencyList(string[] test)
@@ -46,7 +59,7 @@ namespace PluralsightTest
             //Dictionary<string, string> dependencies = new Dictionary<string, string>();
             //List<string> programs = new List<string>();
             //List<string> dependents = new List<string>();
-            List<Node> graph = new List<Node>();
+            List<node> graph = new List<node>();
 
             List<string> noDep = new List<string>();
 
@@ -58,50 +71,58 @@ namespace PluralsightTest
                 string[] temp = test[i].Split(':');
              
                 //Find all that don't have dependents.
-                if (temp[1].Trim() == " ")
+                if (temp[1].Trim() == "")
                 {
                     noDep.Add(temp[0].Trim());
                 }
                 else
                 {
-                    graph.Add(new Node(temp[0].Trim(), temp[1].Trim()));
+                    graph.Add(new node(temp[0].Trim(), temp[1].Trim()));
                 }
             }
 
             //Look for dependents(values) that are not in the programs
-           // foreach(Node entry in graph)
+            // foreach(Node entry in graph)
             //{
             //    if(!graph.Contains(.ContainsKey(entry.Value))
-           //     {
-           //         noEdges.Add(entry.Value);
-           //         dependencies.Remove(entry.Key);
-           //     }
-           // }
-
+            //     {
+            //         noEdges.Add(entry.Value);
+            //         dependencies.Remove(entry.Key);
+            //     }
+            // }
+            Console.WriteLine(noDep.Count);
             while(noDep.Count != 0)
             {
+                Console.Write(noDep.Count);
+                Console.Write("\n");
                 //Grab front string from no Edge List
                 string popped = noDep[0];
                 noDep.Remove(popped);
 
                 //Add to Dependency Final List
-                final += popped;
+                final = final + popped + ", ";
 
-                foreach(Node x in graph)
+                for(int q = 0; q < graph.Count(); q++)
                 {
-                    //Go through graph and find those that have a dependency on the popped 
-                    if(x.dependency == popped)
+                    if(graph[q].dependency == popped)
                     {
-                        noDep.Add(x.program);
-                        graph.Remove(x);
+                        noDep.Add(graph[q].program);
+                        graph.Remove(graph[q]);
+                        q--;
                     }
                 }
             }
 
-            
+            if(graph.Count > 0)
+            {
+                Console.WriteLine(final);
+                Console.WriteLine("\n");
+                Console.WriteLine(graph.Count);
+                return "ERROR: DEPENDENCY CYCLES";
+            }
 
 
-                return "done";
+                return final;
         }
 
     }
