e43d84f6fb6486d5e934afb8e904de0f0c075555
emolytpic <emolyptic@gmail.com>
2018-02-02 19:57:54 -0700

    Push Initial Project

---
+++ PluralsightTest/PluralsightTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluralsightTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] testArray = new string[]
+                {
+                "KittenService: ",
+                "Leetmeme: Cyberportal",
+                "Cyberportal: Ice",
+                "CamelCaser: KittenService",
+                "Fraudstream: Leetmeme",
+                "Ice: "
+                };
+
+            string[] output = createDependencyList(testArray);
+        }
+        public static string[] createDependencyList(string[] test)
+        {
+            return test;
+        }
+
+    }
+}
