ab1366d6ebe482129cbd592a50845316761b701d
emolytpic <emolyptic@gmail.com>
2018-02-02 21:25:58 -0700

    Parsing Strings  and setting up start of Algorithm

--- PluralsightTest/PluralsightTest/Program.cs
+++ PluralsightTest/PluralsightTest/Program.cs
@@ -20,11 +20,58 @@ namespace PluralsightTest
                 "Ice: "
                 };
 
-            string[] output = createDependencyList(testArray);
+            string output = createDependencyList(testArray);
+
+            Console.ReadLine();
         }
-        public static string[] createDependencyList(string[] test)
+        public static string createDependencyList(string[] test)
         {
-            return test;
+            List<string> programs = new List<string>();
+            List<string> dependents = new List<string>();
+
+            List<string> noEdges = new List<string>();
+
+            string final = "";
+
+            //Create two strings one of programs and one of their dependents
+            for (int i = 0; i < test.Length; i++)
+            {
+                string[] temp = test[i].Split(':');
+                programs.Add(temp[0].Trim());
+                dependents.Add(temp[1].Trim());
+
+                //Find all that don't have dependents.
+                if (temp[1] == " ")
+                {
+                    noEdges.Add(temp[0]);
+                }
+            }
+
+            //Look for dependents that are not in the programs
+            for (int y = 0; y < dependents.Count(); y++)
+            {
+                if(!programs.Contains(dependents[y]))
+                {
+                    noEdges.Add(dependents[y]);
+                }
+            }
+
+            while(noEdges.Count != 0)
+            {
+                //Remove from no Edge List
+                string popped = noEdges[0];
+
+                //Add to Dependency Final List
+                final += popped;
+
+               
+
+            }
+
+
+
+
+                return "done";
         }
 
     }
